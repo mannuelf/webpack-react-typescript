587ef0921d4cc79addbd27438581d0ef287ad144
Mannuel Ferreira <mannuelf@users.noreply.github.com>
2021-02-03 10:27:02 +0100

    init

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    "style-loader", // Creates `style` nodes from JS strings
+                    "css-loader",   // Translates CSS into CommonJS
+                    "sass-loader",  // Compiles Sass to CSS
+                ],
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    watch: true,
+}
